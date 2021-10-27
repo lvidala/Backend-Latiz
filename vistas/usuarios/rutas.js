87f8694eb65f9da8c7d35b459bbf85cb4fd666ae
Luisa Vidal <90484155+lvidala@users.noreply.github.com>
2021-10-26 22:45:06 -0500

    Update rutas.js

--- vistas/usuarios/rutas.js
+++ vistas/usuarios/rutas.js
@@ -2,9 +2,7 @@ import Express from 'express';
 import { editarUsuario, 
     queryAllUsers, 
     crearUsuario, 
-    eliminarUsuario, 
-    consultarUsuario 
-    // consultarOCrearUsuario
+    //consultarUsuario 
 } from '../../controladores/usuarios/controlador.js';
 
 const rutasUsuario = Express.Router();
@@ -22,38 +20,36 @@ const genericCallback = (res) => (err, result) => {
 
 //las rutas se manejaron de forma dinámica, para poder ejecutar patch y delete necesitamos un id obligatorio para obtener la información del Usuario específico.
 
-rutasUsuario.route('/usuarios').get((req, res) => {
+
+//GET ALL USERS
+rutasUsuario.route('/').get((req, res) => {
     console.log('alguien hizo get en la ruta /usuarios');
     queryAllUsers(genericCallback(res));
 });
 
-rutasUsuario.route('/usuarios').post((req,res) =>{ 
-  //agregar un nuevo Usuario en la bd, con unos campos obligatorios.
+//CREATE USER
+rutasUsuario.route('/create',(req,res) =>{ 
     crearUsuario(req.body, genericCallback(res));
    
 });
 
-rutasUsuario.route('/usuarios/self').get((req, res) => {
-    console.log('alguien hizo get en la ruta /self');       //ruta self --- donde está la info personal de cada usuario
-    consultarOCrearUsuario(req, genericCallback(res));
-    //consultarUsuario(req.params.id, genericCallback(res));
-});
+// rutasUsuario.route('/usuarios/self').get((req, res) => {
+//     console.log('alguien hizo get en la ruta /self');       //ruta self --- donde está la info personal de cada usuario
+//     consultarOCrearUsuario(req, genericCallback(res));
+//     //consultarUsuario(req.params.id, genericCallback(res));
+// });
 
-rutasUsuario.route('/usuarios/:id').get((req, res) => {
+//SEARCH USER BY ID
+rutasUsuario.get('/:id',(req, res) => {
     console.log('alguien hizo get one user en la ruta /usuarios');
-    consultarUsuario(req.params.id, genericCallback(res));
+    consultarUsuarioPorId(req.params.id, genericCallback(res));
 });
 
-
-rutasUsuario.route('/usuarios/:id').patch((req, res) => {   //
+//EDIT USER BY ID
+rutasUsuario.patch('/edit/:id',(req, res) => {   //
     console.log('alguien hizo patch en la ruta /usuarios/editar');
     editarUsuario(req.params.id, req.body, genericCallback(res));
 
-});
-
-rutasUsuario.route('/usuarios/:id').delete((req, res) => {
-    console.log('alguien hizo delete en la ruta /usuarios/eliminar');
-    eliminarUsuario(req.params.id, genericCallback(res));
 });
 
 export default rutasUsuario;
