3e465e84c9d88cf92d9b6074af6df0d68393428a
Luisa Vidal <90484155+lvidala@users.noreply.github.com>
2021-10-26 22:45:53 -0500

    update rutas.js

--- vistas/productos/rutas.js
+++ vistas/productos/rutas.js
@@ -8,51 +8,51 @@ consultarProductoPorId } from '../../controladores/productos/controlador.js';
 
 const rutasProducto = Express.Router();
 
-const genericCallback = (res) => (err, result) => {
-  //función anidada que puede ser reutilizada en todos los modelos, manda al front un error en forma de status o un resultado como archivo json
-  console.log(err)      
-  if(err) {
-            res.status(err.status).send(err.message)
-        } else {
-            res.json(result);
-        }
-    };
+const genericCallback = (res) => (err, result) => {    
+    if(err) {
+        res.status(err.status).send(err.message)
+    } else {
+        res.json(result);
+    }
+};
+
+//GET ALL PRODUCTS
+rutasProducto.get('/',(req,res)=>{
+    console.log('alguien hizo get en la ruta /productos');    
+    queryAllProducts(req.query,genericCallback(res))
+})
 
 
-//las rutas se manejaron de forma dinámica, para poder ejecutar patch y delete necesitamos un id obligatorio para obtener la información del producto específico.
-
-rutasProducto.route('/productos').get((req, res) => {
-    console.log('alguien hizo get en la ruta /productos');
-    queryAllProducts(genericCallback(res));
-});
-
-rutasProducto.route('/productos').post((req,res) =>{ 
-  //agregar un nuevo producto en la bd, con unos campos obligatorios.
+//CREATE A PRODUCT
+rutasProducto.post('/create',(req,res)=>{
     crearProducto(req.body, genericCallback(res));
-   
-});
+})
 
-rutasProducto.route('/productos/:id').get((req, res) => {
+
+// SEARCH PRODUC BY ID
+rutasProducto.get('/:id',(req, res) => {
     console.log('alguien hizo get one product en la ruta /productos');
     consultarProductoPorId(req.params.id, genericCallback(res));
 });
 
-rutasProducto.route('/productos/name').get((req, res) => {
+// SEARCH PRODUC BY NAME
+rutasProducto.get('/:name',(req, res) => {
     console.log('alguien hizo get one product en la ruta /productos');
-    console.log(URL.parse(req.url))
-    // consultarProductoPorNombre(req.query.name, genericCallback(res));
-}); //cambiar por product
+    consultarProductoPorNombre(req.params.id, genericCallback(res));
+});
 
 
-rutasProducto.route('/productos/:id').patch((req, res) => {   //
+//EDIT ELEMENT BY ID
+rutasProducto.patch('/edit/:id',(req, res) => {   //
     console.log('alguien hizo patch en la ruta /productos/editar');
     editarProducto(req.params.id, req.body, genericCallback(res));
-
 });
 
-rutasProducto.route('/productos/:id').delete((req, res) => {
+//Delete PRODUCT BY ID
+rutasProducto.delete('/delete/:id',(req,res)=>{
     console.log('alguien hizo delete en la ruta /productos/eliminar');
     eliminarProducto(req.params.id, genericCallback(res));
-});
+})
+
 
 export default rutasProducto;
