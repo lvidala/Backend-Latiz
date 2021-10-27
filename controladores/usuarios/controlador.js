dd7a71fa827d0bf12fd323ae3f0c3ed65c13dc87
Luisa Vidal <90484155+lvidala@users.noreply.github.com>
2021-10-26 22:47:49 -0500

    Update controlador.js

--- controladores/usuarios/controlador.js
+++ controladores/usuarios/controlador.js
@@ -4,7 +4,7 @@ import { ObjectId } from 'mongodb';
 import { response } from 'express';
 
 
-const queryAllUsers = async (callback) => {  //controlador que muestra todos los Usuarios.
+export const queryAllUsers = async (callback) => {  //controlador que muestra todos los Usuarios.
 const baseDeDatos = getDB();
 await baseDeDatos
 .collection('users')
@@ -13,12 +13,12 @@ await baseDeDatos
 .toArray(callback);
 };
 
-const crearUsuario = async (datosUsuario, callback) => {
+export const crearUsuario = async (datosUsuario, callback) => {
     const baseDeDatos = getDB();
     await baseDeDatos.collection('users').insertOne(datosUsuario, callback);
   };
 
-const consultarUsuario = async (id,callback) => { //controlador que muestra un solo Usuario.
+export const consultarUsuario = async (id,callback) => { //controlador que muestra un solo Usuario.
     const baseDeDatos = getDB(); 
 await baseDeDatos
 .collection('users')
@@ -47,7 +47,7 @@ await baseDeDatos
 //   };
 
 
-const editarUsuario = async (id, edicion, callback) => {
+export const editarUsuario = async (id, edicion, callback) => {
     const filtroUsuario = { _id: new ObjectId(id) };
     const operacion = {
         $set: edicion,
@@ -59,10 +59,3 @@ const editarUsuario = async (id, edicion, callback) => {
     .findOneAndUpdate(filtroUsuario, operacion, {upsert: true, returnOriginal: true}, callback);
 };
 
-const eliminarUsuario = async (id, callback) => {
-    const filtroUsuario = { _id: new ObjectId(id) };
-    const baseDeDatos = getDB();
-    await baseDeDatos.collection('users').deleteOne(filtroUsuario, callback);
-  };
-
-export { queryAllUsers, crearUsuario, editarUsuario, eliminarUsuario, consultarUsuario }; //consultarOCrearUsuario}; 
