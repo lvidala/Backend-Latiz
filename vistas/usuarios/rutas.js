d2c3e071bd9fade5bfcdae40da600b7641bf52b1
lvidala <luisiva011@gmail.com>
2021-10-27 15:07:37 -0500

    update routes

--- vistas/usuarios/rutas.js
+++ vistas/usuarios/rutas.js
@@ -1,14 +1,8 @@
 import Express from 'express';
 import { editarUsuario, 
-<<<<<<< HEAD
     listarUsuarios, 
     crearUsuario,  
     consultarUsuario 
-=======
-    queryAllUsers, 
-    crearUsuario, 
-    //consultarUsuario 
->>>>>>> 0e83f63e9d7f81c450c5b6da303a4b0a658739d6
 } from '../../controladores/usuarios/controlador.js';
 
 const rutasUsuario = Express.Router();
@@ -26,67 +20,36 @@ const genericCallback = (res) => (err, result) => {
 
 //las rutas se manejaron de forma dinámica, para poder ejecutar patch y delete necesitamos un id obligatorio para obtener la información del Usuario específico.
 
-<<<<<<< HEAD
+//GET ALL USERS
 rutasUsuario.route('/users').get((req, res) => {
     console.log('alguien hizo get en la ruta /users');
     listarUsuarios(genericCallback(res));
 });
 
+//CREATE AN USER
 rutasUsuario.route('/users/create').post((req,res) =>{ 
-  //agregar un nuevo Usuario en la bd, con unos campos obligatorios.
-=======
-
-//GET ALL USERS
-rutasUsuario.route('/').get((req, res) => {
-    console.log('alguien hizo get en la ruta /usuarios');
-    queryAllUsers(genericCallback(res));
-});
-
-//CREATE USER
-rutasUsuario.route('/create',(req,res) =>{ 
->>>>>>> 0e83f63e9d7f81c450c5b6da303a4b0a658739d6
     crearUsuario(req.body, genericCallback(res));
    
 });
 
-<<<<<<< HEAD
+
 rutasUsuario.route('/users/self').get((req, res) => {
     console.log('alguien hizo get en la ruta /self');       //ruta self --- donde está la info personal de cada usuario
     consultarOCrearUsuario(req, genericCallback(res));
     //consultarUsuario(req.params.id, genericCallback(res));
 });
 
+//GET USER BY ID
 rutasUsuario.route('/users/:id').get((req, res) => {
     console.log('alguien hizo get one user en la ruta /users');
     consultarUsuario(req.params.id, genericCallback(res));
 });
 
-
+//EDIT USER
 rutasUsuario.route('/users/edit/:id').patch((req, res) => {   //
     console.log('alguien hizo patch en la ruta /users/editar');
-=======
-// rutasUsuario.route('/usuarios/self').get((req, res) => {
-//     console.log('alguien hizo get en la ruta /self');       //ruta self --- donde está la info personal de cada usuario
-//     consultarOCrearUsuario(req, genericCallback(res));
-//     //consultarUsuario(req.params.id, genericCallback(res));
-// });
-
-//SEARCH USER BY ID
-rutasUsuario.get('/:id',(req, res) => {
-    console.log('alguien hizo get one user en la ruta /usuarios');
-    consultarUsuarioPorId(req.params.id, genericCallback(res));
-});
-
-//EDIT USER BY ID
-rutasUsuario.patch('/edit/:id',(req, res) => {   //
-    console.log('alguien hizo patch en la ruta /usuarios/editar');
->>>>>>> 0e83f63e9d7f81c450c5b6da303a4b0a658739d6
     editarUsuario(req.params.id, req.body, genericCallback(res));
 
 });
 
-<<<<<<< HEAD
 export default rutasUsuario;
-=======
-export default rutasUsuario;
->>>>>>> 0e83f63e9d7f81c450c5b6da303a4b0a658739d6
--- vistas/ventas/rutas.js
+++ vistas/ventas/rutas.js
@@ -19,48 +19,27 @@ const genericCallback = (res) => (err, result) => {
 
 //las rutas se manejaron de forma dinámica, para poder ejecutar patch y delete necesitamos un id obligatorio para obtener la información del Venta específico.
 
-<<<<<<< HEAD
+//GET ALL SALES
 rutasVenta.route('/sales').get((req, res) => {
     console.log('alguien hizo get en la ruta /sales');
     listarVentas(genericCallback(res));
 });
 
+//CREATE NEW SALE
 rutasVenta.route('/sales/create').post((req,res) =>{ 
-=======
-
-//GET ALL SALES
-rutasVenta.get('/',(req, res) => {
-    console.log('alguien hizo get en la ruta /ventas');
-    queryAllSales(genericCallback(res));
-});
-
-
-//CREATE A SALE
-rutasVenta.post('/create',(req,res) =>{ 
->>>>>>> 0e83f63e9d7f81c450c5b6da303a4b0a658739d6
     crearVenta(req.body, genericCallback(res));
    
 });
 
-<<<<<<< HEAD
+//GET SALE BY ID
 rutasVenta.route('/sales/:id').get((req, res) => {
     console.log('alguien hizo get one user en la ruta /sales');
     consultarVenta(req.params.id, genericCallback(res));
 });
 
-
+//EDIT SALE BY ID
 rutasVenta.route('/sales/edit/:id').patch((req, res) => {   //
     console.log('alguien hizo patch en la ruta /sales/editar');
-=======
-//SEARCH SALE 
-rutasVenta.get('/:id',(req, res) => {
-    consultarVenta(req.params.id, genericCallback(res));
-});
-
-//EDIT SALE
-rutasVenta.route('/edit/:id',(req, res) => {   //
-    console.log('alguien hizo patch en la ruta /ventas/editar');
->>>>>>> 0e83f63e9d7f81c450c5b6da303a4b0a658739d6
     editarVenta(req.params.id, req.body, genericCallback(res));
 
 });
